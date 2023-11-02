36e020076ceeb0e6cecec2d05a1f37f35d034da4
Jerome Eteve <jeteve@users.noreply.github.com>
2023-11-02 19:17:23 +0000

    Skip legacy build. Build latest versions

--- build.sh
+++ build.sh
@@ -5,19 +5,27 @@ set -xe
 # WGET_OPTS=--no-check-certificate
 
 # Legacy version.
-TAO_VERSION=6.2.0
- docker build \
-  --build-arg BUILD_IMAGE=ubuntu:precise-20150320 \
-  --build-arg LEGACY=1 \
-  --build-arg TAO_VERSION=$TAO_VERSION \
-  --build-arg WGET_OPTS=$WGET_OPTS \
-  -t jeteve/tao_ace:$TAO_VERSION .
+# TAO_VERSION=6.2.0
+#  docker build \
+#   --build-arg BUILD_IMAGE=ubuntu:precise-20150320 \
+#   --build-arg LEGACY=1 \
+#   --build-arg TAO_VERSION=$TAO_VERSION \
+#   --build-arg WGET_OPTS=$WGET_OPTS \
+#   -t jeteve/tao_ace:$TAO_VERSION .
 
 # exit 0
 
+TAO_VERSION=7.1.0
+docker build \
+  --build-arg BUILD_IMAGE=ubuntu:jammy \
+  --build-arg TAO_VERSION=$TAO_VERSION \
+  --build-arg WGET_OPTS=$WGET_OPTS \
+  -t jeteve/tao_ace:$TAO_VERSION\
+   .
+
 
 # Latest release
-TAO_VERSION=7.1.0
+TAO_VERSION=7.1.2
 docker build \
   --build-arg BUILD_IMAGE=ubuntu:jammy \
   --build-arg TAO_VERSION=$TAO_VERSION \
